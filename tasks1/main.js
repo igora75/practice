b98b4088867aec256a370bb4edcf31cfdc04b4a6
Ihor Karpov <igora75@gmail.com>
2020-07-07 10:14:17 +0300

    alpha

---
+++ tasks1/main.js
@@ -0,0 +1,348 @@
+// 1, 2
+// let a1 = 'hello',
+//     a2 = 'owu',
+//     a3 = 'com',
+//     a4 = 'ua',
+//     a5 = 1,
+//     a6 = 10,
+//     a7 = -999,
+//     a8 = 123,
+//     a9 = 3.14,
+//     a10 = 2.7,
+//     a11 = 16,
+//     a12 = true,
+//     a13 = false
+
+// a1 = 'hi'
+// a2 = 7
+// a3 = 'com.ua'
+// a4 = true
+// a5 = 45
+// a6 = '123'
+// a7 = -9
+// a8 = 12.3
+// a9 = Math.PI
+// a10 = '2.7'
+// a11 = 160
+// a12 = 11
+// a13 = 0
+
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+// console.log(a6);
+// console.log(a7);
+// console.log(a8);
+// console.log(a9);
+// console.log(a10);
+// console.log(a11);
+// console.log(a12);
+// console.log(a13);
+
+// alert(a1);
+// alert(a2);
+// alert(a3);
+// alert(a4);
+// alert(a5);
+// alert(a6);
+// alert(a7);
+// alert(a8);
+// alert(a9);
+// alert(a10);
+// alert(a11);
+// alert(a12);
+// alert(a13);
+
+// document.write(a1);
+// document.write(a2);
+// document.write(a3);
+// document.write(a4);
+// document.write(a5);
+// document.write(a6);
+// document.write(a7);
+// document.write(a8);
+// document.write(a9);
+// document.write(a10);
+// document.write(a11);
+// document.write(a12);
+// document.write(a13);
+
+//3
+// const b1 = 3,
+//     b2 = 45,
+//     b3 = 3.333,
+//     b4 = 'vasia',
+//     b5 = '123',
+//     b6 = 'hi'
+
+// console.log(b1);
+// console.log(b2);
+// console.log(b3);
+// console.log(b4);
+// console.log(b5);
+// console.log(b6);
+
+// alert(b1);
+// alert(b2);
+// alert(b3);
+// alert(b4);
+// alert(b5);
+// alert(b6);
+
+// document.write(b1);
+// document.write(b2);
+// document.write(b3);
+// document.write(b4);
+// document.write(b5);
+// document.write(b6);
+
+// 4, 5, 6
+// let name = prompt('Enter name'),
+//     surname1 = prompt('Enter surname1'),
+//     surname2 = prompt('Enter surname2')
+
+// console.log(name);
+// console.log(surname1);
+// console.log(surname2);
+
+// alert(name);
+// alert(surname1);
+// alert(surname2);
+
+// document.write(name);
+// document.write(surname1);
+// document.write(surname2);
+
+// let person = name + ' ' + surname1 + ' ' + surname2;
+// console.log(person);
+
+// 7
+// let a1 = +prompt('Число1'),
+//     a2 = +prompt('Число2'),
+//     a3 = +prompt('Число3')
+
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+
+// 8
+// let b1 = parseInt(prompt('Число1')),
+//     b2 = parseInt(prompt('Число2')),
+//     b3 = parseInt(prompt('Число3')),
+//     b4 = parseInt(prompt('Число4'))
+
+// let result = b1 + b2 + b3 + b4;
+// console.log(result);
+
+// 9
+// let b1 = parseFloat(prompt('Число1')),
+//     b2 = parseFloat(prompt('Число2')),
+//     b3 = parseFloat(prompt('Число3'))
+
+// let result = b1 + b2 + b3;
+// console.log(result);
+
+// 10
+// let b1 = Math.round(prompt('Число1')),
+//     b2 = Math.round(prompt('Число2')),
+//     b3 = Math.round(prompt('Число3'))
+
+// let result = b1 + b2 + b3;
+// console.log(result);
+
+// 11
+// let b1 = Math.round(+prompt('Число1')),
+//     b2 = Math.round(+prompt('Число2'))
+
+// console.log(Math.pow(b1, b2));
+
+// 12
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+
+// 13 - 26
+// let a1 = 5 < 6;
+// console.log(a1);
+
+// let a2 = 5 > 6;
+// console.log(a2);
+
+// let a3 = 5 >= 6;
+// console.log(a3);
+
+// let a4 = 5 === 6;
+// console.log(a4);
+
+// let b1 = 10 === 10;
+// console.log(b1);
+
+// let b2 = 10 == 10;
+// console.log(b2);
+
+// let b3 = 10 !== 10;
+// console.log(b3);
+
+// let b4 = 10 > 10;
+// console.log(b4);
+
+// let b5 = 10 != 10;
+// console.log(b5);
+
+// let c1 = 123 === '123';
+// console.log(c1);
+
+// let c2 = 123 == '123';
+// console.log(c2);
+
+// Add
+// console.log(132 > 100 && 45 < 12); //false
+// console.log(34 > 33 && 23 < 90); //true
+// console.log(99 > 100 && 45 > 12); //false
+// console.log(132 > 100 || 45 < 12); //true
+// console.log(111 > 11 || 45 < 111); //true
+// console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12)); //true
+// console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12)); //true
+// console.log((132 > 100 && 45 < 12) && (132 > 100 || 45 < 12)); //false
+// console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12)); //true
+// console.log(!!'-1'); //true
+// console.log(!!-1); //true
+// console.log(!!'0'); //true
+// console.log(!!'null'); //true
+// console.log(!!'undefined'); //true
+// console.log(!!(3 / 'owu')); //false
+// console.log((111 > 11 || 45 < 111) || !!'0'); //true
+// console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12)); //false
+
+// CLASS
+
+//1
+// let str = 'Привет',
+//     num = 123,
+//     flag = true,
+//     txt = 'true'
+
+// console.log(typeof str);
+// console.log(typeof num);
+// console.log(typeof flag);
+// console.log(typeof txt);
+
+//2
+// let a1 = 5 + 3,
+//     a2 = 5 - 3,
+//     a3 = 5 * 3,
+//     a4 = 5 / 3,
+//     a5 = 5 % 3
+
+//3
+// let a6 = 5 % 3,
+//     a7 = 3 % 5,
+//     a8 = 5 + '3',
+//     a9 = '5' - 3,
+//     a10 = 75 + 'кг'
+
+// //4
+// let hight = 23,
+//     width = 10
+
+// let s=hight*width;
+// console.log(s);
+
+// //5
+// let hightC = 10,
+//     dC = 4
+
+// let v=Math.PI*dC**2*hightC/4;
+// console.log(v)
+
+//6
+// let n = 3,
+//     m = 4
+
+// let k = Math.sqrt(n ** 3 + m ** 4);
+// console.log(k);
+
+//7
+// let str='Hello world';
+
+// alert(str);
+// document.write(str);
+// console.log(str);
+
+//8
+// alert('Карпов І.О.\n43 роки\nПрограміст')
+
+//9
+// let str1 = 'Хто',
+//     str2 = 'ти',
+//     str3 = 'такий?'
+
+// let concatenation = str1 + ' ' + str2 + ' ' + str3;
+// document.write(concatenation);
+
+//10
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>"); //205
+// document.write(str - a + "<br/>"); //15
+// document.write(str * "2" + "<br/>"); //40
+// document.write(str / 2 + "<br/>"); //10
+
+//11
+// parseInt("3.14") //3
+// parseInt("-7.875") //-7
+// parseInt("435") //435
+// console.log(parseInt("Вася")) //NaN
+
+//12
+// let str = prompt("Enter something", "ho-ho")
+// console.log(str);
+
+//13
+// let a1 = +prompt('число 1'),
+//     a2 = +prompt('число 2')
+
+// alert(a1 + a2);
+
+//14
+// let a = prompt('Enter name'),
+//     b = prompt('Enter surname'),
+//     c = prompt('Enter age')
+
+// alert(`Доброго вечера ${a} ${b}, мои поздравления что вам ${c}`)
+
+//Dop//
+
+//1
+// let a = +prompt('Enter num1'),
+//     b = +prompt('Enter num2'),
+//     c = +prompt('Enter num3')
+
+// if (a > b) {
+//     a = a + b;
+//     b = a - b;
+//     a = a - b;
+// }
+// if (b > c) {
+//     b = b + c;
+//     c = b - c;
+//     b = b - c;
+// }
+// if (a > b) {
+//     a = a + b;
+//     b = a - b;
+//     a = a - b;
+// }
+
+// document.write(`${a} ${b} ${c}`);
+
+
+
