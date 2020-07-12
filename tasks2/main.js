843c0bbc26b09f2f08ade1ac117c078c854869e5
Ihor Karpov <igora75@gmail.com>
2020-07-12 13:01:57 +0300

    ggg

---
+++ tasks2/main.js
@@ -0,0 +1,47 @@
+//2
+// let arr = [4, 5, 3.14, 67, -8];
+// console.log(arr);
+
+//3
+// let arr = ['fsf', '45', '', '2.3', 'yiu'];
+// console.log(arr);
+
+//4
+// let arr = ['fsf', 45, false, '2.3', -65];
+// console.log(arr);
+
+//5
+// let arr = [];
+// arr[0] = true;
+// arr[1] = 'fhg';
+// arr[2] = 3.14;
+// arr[3] = -45;
+// arr[4] = '0.123';
+
+// console.log(arr);
+
+//7
+// for (i = 0; i < 10; i++) {
+//     document.write('<div>lorem</div>');
+// }
+
+//8
+// for (i = 0; i < 10; i++) {
+//     document.write(`<div>lorem${i + 1}</div>`);
+// }
+
+//9
+// let i = 0;
+// while (i < 20) {
+//     document.write('<h1>lorem</h1>');
+//     i++;
+// }
+
+//10
+// let i = 0;
+// while (i < 20) {
+//     document.write(`<h1>lorem${i + 1}</h1>`);
+//     i++;
+// }
+
+//11
