20cd338fc4c67ad879b0301ad2738d8d78cef172
Ihor Karpov <igora75@gmail.com>
2020-07-14 21:56:55 +0300

    mid

--- tasks3/task3.1/template2.js
+++ tasks3/task3.1/template2.js
@@ -8,3 +8,31 @@ let allWidth = document.getElementsByClassName('linkList');
 allWidth[0].style.width = '50%';
 allWidth[1].style.width = '50%';
 allWidth[2].style.width = '50%';
+
+let txtList2 = document.getElementsByClassName('listElement2');
+console.log(txtList2[0].innerText);
+
+let allLi = document.getElementsByTagName('li');
+allLi[0].style.backgroundColor = 'gray';
+allLi[1].style.backgroundColor = 'gray';
+allLi[2].style.backgroundColor = 'gray';
+
+let addAnchor = document.getElementsByTagName('a');
+addAnchor[0].classList.add('anchor');
+addAnchor[1].classList.add('anchor');
+addAnchor[2].classList.add('anchor');
+
+let bigText = document.getElementsByTagName('a');
+for (let i = 0; i < bigText.length; i++) {
+    if (bigText[i].innerText === 'link3') {
+        bigText[i].style.fontSize = '40px';
+    }
+}
+
+let addClass = document.getElementsByTagName('a');
+for (let i = 0; i < bigText.length; i++) {
+    addClass[i].classList.add('element_' + addClass[i].innerText);
+}
+
+
+
