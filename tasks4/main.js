5fde647d6409d41d1ba7caa0746a4478826678a4
Ihor Karpov <igora75@gmail.com>
2020-07-21 16:03:32 +0300

    finished changes

---
+++ tasks4/main.js
@@ -0,0 +1,13 @@
+//1***************************************
+function showArr(arr) {
+    console.log(arr);
+};
+
+//2***************************************
+function randArr(arr) {
+    let arrR= arr.push(Math.random);
+    showArr(arrR)
+};
+
+let arr=[];
+arr=randArr(arr);
