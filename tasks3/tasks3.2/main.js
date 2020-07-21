8bd6ba7f619b25c8026608803f59a72183b89846
Ihor Karpov <igora75@gmail.com>
2020-07-21 09:28:16 +0300

    ended task3

--- tasks3/tasks3.2/main.js
+++ tasks3/tasks3.2/main.js
@@ -227,9 +227,209 @@ console.log(arrUsers10);
 //     document.body.append(div);
 // }
 
+//27*******************************************************
 for (let user10 of users10) {
     let div = document.createElement('div');
-    let div2 = document.createElement('div');
-    div.append(div2);
+
+    let divName = document.createElement('div');
+    divName.innerText = user10.name;
+
+    let divAge = document.createElement('div');
+    divAge.innerText = user10.age;
+
+    let divStatus = document.createElement('div');
+    divStatus.innerText = user10.status;
+
+    let divAddress = document.createElement('div');
+
+    let divCity = document.createElement('div');
+    divCity.innerText = user10.address.city;
+
+    let divCountry = document.createElement('div');
+    divCountry.innerText = user10.address.country;
+
+    let divStreet = document.createElement('div');
+    divStreet.innerText = user10.address.street;
+
+    let divNumber = document.createElement('div');
+    divNumber.innerText = user10.address.houseNumber;
+
+    divAddress.append(divCity, divCountry, divStreet, divNumber);
+    div.append(divName, divAge, divStatus, divAddress);
     document.body.append(div);
+}
+
+//87*******************************************************
+let usersWithId = [{ id: 1, name: 'vasya', age: 31, status: false }, { id: 2, name: 'petya', age: 30, status: true }, { id: 3, name: 'kolya', age: 29, status: true }, { id: 4, name: 'olya', age: 28, status: false },];
+let citiesWithId = [{ user_id: 3, country: 'USA', city: 'Portland' }, { user_id: 1, country: 'Ukraine', city: 'Ternopil' }, { user_id: 2, country: 'Poland', city: 'Krakow' }, { user_id: 4, country: 'USA', city: 'Miami' },];
+let arr1 = [];
+
+for (let userId of usersWithId) {
+    for (let citiesId of citiesWithId) {
+        if (userId.id === citiesId.user_id) {
+            userId.address = citiesId;
+            arr1.push(userId);
+        }
+    }
+}
+console.log(arr1);
+
+//100*****************************************************
+let div = document.createElement('div');
+div.id = '#1';
+div.className = 'block';
+div.innerText = 'My block';
+
+document.body.append(div);
+
+let a = document.getElementById('#1');
+a.innerText = 'Modify block';
+a.style.height = '50px';
+a.style.width = '50px';
+console.log(a.innerText);
+
+let b = document.getElementsByClassName('block');
+b[0].innerText = 'Modify block2';
+a.style.height = '50px';
+a.style.width = '50px';
+console.log(b[0].innerText);
+
+// let c = document.getElementsByTagName('div');
+// c[0].innerText = 'Modify block3';
+// a.style.height = '50px';
+// a.style.width = '50px';
+// console.log(c[0].innerText);
+
+//103***********************************************
+let table1 = document.createElement('table');
+
+let tr1 = document.createElement('tr');
+
+let td1 = document.createElement('td');
+td1.innerText = '11111';
+let td2 = document.createElement('td');
+td2.innerText = '22222';
+let td3 = document.createElement('td');
+td3.innerText = '33333';
+
+table1.append(tr1);
+tr1.append(td1, td2, td3);
+
+document.body.append(table1);
+
+//104*************************************************
+let table2 = document.createElement('table');
+for (let i = 0; i < 10; i++) {
+    let tr2 = document.createElement('tr');
+    table2.append(tr2);
+
+    let td1 = document.createElement('td');
+    td1.innerText = 'aaaaa';
+    let td2 = document.createElement('td');
+    td2.innerText = 'bbbbb';
+    let td3 = document.createElement('td');
+    td3.innerText = 'ccccc';
+
+    tr2.append(td1, td2, td3);
+}
+document.body.append(table2);
+
+//105***********************************************
+let table3 = document.createElement('table');
+
+for (let i = 0; i < 10; i++) {
+    let tr3 = document.createElement('tr');
+    table3.append(tr3);
+    for (let j = 0; j < 5; j++) {
+        let tdN = document.createElement('td');
+        tdN.innerText = '33333';
+        tr3.append(tdN);
+    }
+}
+document.body.append(table3);
+
+//106****************************************************
+// let N = +prompt('N=');
+// let M = +prompt('M=');
+// let table4 = document.createElement('table');
+
+// for (let i = 0; i < N; i++) {
+//     let tr4 = document.createElement('tr');
+//     table4.append(tr4);
+//     for (let j = 0; j < M; j++) {
+//         let tdN = document.createElement('td');
+//         tdN.innerText = '33333';
+//         tr4.append(tdN);
+//     }
+// }
+// document.body.append(table4);
+
+//109****************************************************
+// let classElements = document.getElementsByTagName('*');
+// for (let classElem of classElements) {
+//     if (classElem.className) {
+//         console.log(classElem);
+//     }
+// }
+
+//110*****************************************************
+// let allPs = document.getElementsByTagName('p');
+// for (let allP of allPs) {
+//     allP.innerText = 'hello oktenweb!';
+// }
+
+//111*****************************************************
+// let allDivs = document.getElementsByTagName('div');
+// for (let allDiv of allDivs) {
+//     allDiv.style.color = 'red';
+// }
+
+//121*******************************************************
+let rules = [
+    {
+        title: 'Первое правило Бойцовского клуба.',
+        body: 'Никому не рассказывать о Бойцовском клубе.'
+    },
+    {
+        title: 'Второе правило Бойцовского клуба.',
+        body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+    },
+    {
+        title: 'Третье правило Бойцовского клуба.',
+        body: 'В схватке участвуют только двое.'
+    },
+    {
+        title: 'Четвертое правило Бойцовского клуба.',
+        body: 'Не более одного поединка за один раз.'
+    },
+    {
+        title: 'Пятое правило Бойцовского клуба.',
+        body: 'Бойцы сражаются без обуви и голые по пояс.'
+    },
+    {
+        title: 'Шестое правило Бойцовского клуба.',
+        body: 'Поединок продолжается столько, сколько потребуется.'
+    },
+    {
+        title: 'Седьмое правило Бойцовского клуба.',
+        body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+    },
+    {
+        title: 'Восьмое и последнее правило Бойцовского клуба.',
+        body: 'Новичок обязан принять бой.'
+    },
+
+];
+
+for (let rule of rules) {
+    let wrap = document.createElement('div');
+
+    let innerDiv = document.createElement('div');
+    innerDiv.innerText = rule.title;
+    let innerBody = document.createElement('div');
+    innerBody.innerText = rule.body;
+
+    wrap.append(innerDiv, innerBody);
+
+    document.body.append(wrap);
 }
