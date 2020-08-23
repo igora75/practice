a55919bf4c6071cd84c5c7db3004399dbc80313a
Ihor Karpov <igora75@gmail.com>
2020-08-23 18:07:20 +0300

    ended

--- tasks8/main.js
+++ tasks8/main.js
@@ -51,45 +51,45 @@
 // Требование : хранить историю своих изменений (даже после перезагрузки страницы).
 // Сверху над текстареа должны появится стрелочки, 
 //с помощью которых можно перемещаться по истории (не забудьте!чекпоинт истории - нажатеи кнопки сохранить).
-let lbtn1 = document.querySelector('.lbtn');
-let rbtn1 = document.querySelector('.rbtn');
+// let lbtn1 = document.querySelector('.lbtn');
+// let rbtn1 = document.querySelector('.rbtn');
 
-let txt1 = document.querySelector('.txt');
-let saveBtn1 = document.querySelector('.saveBtn');
+// let txt1 = document.querySelector('.txt');
+// let saveBtn1 = document.querySelector('.saveBtn');
 
-saveBtn1.onclick = () => {
-  localStorage.setItem(localStorage.length + 1, txt1.value);
-}
+// saveBtn1.onclick = () => {
+//   localStorage.setItem(localStorage.length + 1, txt1.value);
+// }
 
-lbtn1.onclick = () => {
-  rbtn1.hidden = false;
-  let index;
-  for (let key in localStorage) {
-    if (localStorage.getItem(key) === txt1.value) {
-      index = key;
-    }
-  }
-  if (index === '1') {
-    lbtn1.hidden = true;
-    return;
-  }
-  txt1.value = localStorage.getItem(index - 1);
-}
+// lbtn1.onclick = () => {
+//   rbtn1.hidden = false;
+//   let index;
+//   for (let key in localStorage) {
+//     if (localStorage.getItem(key) === txt1.value) {
+//       index = key;
+//     }
+//   }
+//   if (index === '1') {
+//     lbtn1.hidden = true;
+//     return;
+//   }
+//   txt1.value = localStorage.getItem(index - 1);
+// }
 
-rbtn1.onclick = () => {
-  lbtn1.hidden = false;
-  let index;
-  for (let key in localStorage) {
-    if (localStorage.getItem(key) === txt1.value) {
-      index = key;
-    }
-  }
-  if (index === localStorage.length.toString()) {
-    rbtn1.hidden = true;
-    return;
-  }
-  txt1.value = localStorage.getItem(+index + 1);
-}
+// rbtn1.onclick = () => {
+//   lbtn1.hidden = false;
+//   let index;
+//   for (let key in localStorage) {
+//     if (localStorage.getItem(key) === txt1.value) {
+//       index = key;
+//     }
+//   }
+//   if (index === localStorage.length.toString()) {
+//     rbtn1.hidden = true;
+//     return;
+//   }
+//   txt1.value = localStorage.getItem(+index + 1);
+// }
 
 // - Реализуйте записную книгу, хранящую данные в локальном хранилище.
 // Данные которые надо сохранять : ФИО, номер, почта, фирма, отдел, день рождения
@@ -98,4 +98,112 @@ rbtn1.onclick = () => {
 // --Каждому контакту добавить кнопку редактироваиня.
 // При нажати на нее появляется форма, в которой есть все необходимые инпуты для редактирования,
 // которые уже заполнены данными объекта
+const ARRAY_USERS = 'ARRAY_USERS';
+const content = document.querySelector('.content');
+let form2 = document.querySelector('.form2');
 
+form2.submit1.onclick = () => {
+    let person = {};
+    for (let i = 0; i < form2.children.length; i++) {
+        const formEl = form2.children[i];
+        if (formEl.name && formEl.tipe !== 'submit') {
+            person[formEl.name] = formEl.value;
+        }
+    }
+
+    person.id = new Date().getTime();
+    saveUser(person);
+}
+
+getDataFromLS();
+
+function saveUser(user) {
+    if (localStorage.hasOwnProperty(ARRAY_USERS)) {
+        const arrayUsers = JSON.parse(localStorage.getItem(ARRAY_USERS));
+        const find = arrayUsers.find(val => val.id === user.id);
+        if (find) {
+            const filter = arrayUsers.filter(val => val.id !== user.id);
+            filter.push(user);
+
+            localStorage.setItem(ARRAY_USERS, JSON.stringify(filter));
+        } else {
+            arrayUsers.push(user);
+
+            localStorage.setItem(ARRAY_USERS, JSON.stringify(arrayUsers));
+        }
+    } else {
+        localStorage.setItem(ARRAY_USERS, JSON.stringify([user]));
+    }
+}
+
+function getDataFromLS() {
+    if (localStorage.hasOwnProperty(ARRAY_USERS)) {
+        const arrUser = JSON.parse(localStorage.getItem(ARRAY_USERS));
+        for (const user of arrUser) {
+            content.append(createDivPerson(user));
+        }
+    }
+}
+
+function createDivPerson(user) {
+    const main = document.createElement('div');
+    let flag = true;
+    for (const key in user) {
+        if (flag) {
+            const h3 = document.createElement('h3');
+            h3.innerText = key + ' : ' + user[key];
+            main.append(h3);
+            flag = false;
+        } else {
+            const p = document.createElement('p');
+            p.innerText = key + ' : ' + user[key];
+            main.append(p);
+        }
+    }
+    main.style = 'width: 300px; border: 1px solid red; float: left';
+
+    const b1 = document.createElement('button');
+    const b2 = document.createElement('button');
+
+    b1.innerText = 'Edit';
+    b2.innerText = 'Delete';
+
+    b1.onclick = function () {
+        editUser(user.id);
+    }
+    b2.onclick = function () {
+        delUser(user.id);
+    }
+
+    main.append(b1);
+    main.append(b2);
+
+    return main;
+}
+
+function delUser(id) {
+    const parse = JSON.parse(localStorage.getItem(ARRAY_USERS));
+    const filter = parse.filter(user => user.id !== id);
+
+    localStorage.setItem(ARRAY_USERS, JSON.stringify(filter));
+    location.reload();
+}
+
+function editUser(id) {
+    const parse = JSON.parse(localStorage.getItem(ARRAY_USERS));
+    const user = parse.find(user => user.id === id);
+
+
+    for (let i = 0; i < form2.children.length; i++) {
+        const formEl = form2.children[i];
+        if (formEl.name && formEl.tipe !== 'submit') {
+            for (const key in user) {
+                if (formEl.name === key) {
+                    formEl.value = user[key];
+                }
+            }
+        }
+    }
+
+
+}
