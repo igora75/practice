e9689017f019e374e0e5caa9c193f343d1ee319d
Ihor Karpov <igora75@gmail.com>
2021-01-02 00:41:37 +0200

    main.hbs

--- nodejs/lesson2/app.js
+++ nodejs/lesson2/app.js
@@ -1,9 +1,6 @@
-const usersArr = [
-  { name: 'Ivan', age: 22 },
-  { name: 'Ira', age: 17 },
-];
 const express = require('express');
 const expressHbs = require('express-handlebars');
+const fs = require('fs');
 // const path = require('path');
 
 const app = express();
@@ -12,11 +9,37 @@ app.use(express.json());
 // app.use(express.static(path.join(__dirname, 'views')));
 app.engine('.hbs', expressHbs({ defaultLayout: false }));
 app.set('view engine', '.hbs');
-app.get('/user', (req, res) => {
-  res.render('users', { users: usersArr });
-});
-// app.get('/', (req, res) => {
-//
+// app.get('/user', (req, res) => {
+//   res.render('users', { users: usersArr });
 // });
-app.listen(3000, () => {
+// app.get('/', (req, res) => {
+//   res.render('main', { isOk: true, userName: 'Ivan' });
+// });
+// app.post('/login', ((req, res) => {
+//   const { name, age } = req.body;
+//   usersArr.push({ name, age });
+//   res.redirect('/user');
+// }));
+let dataFile = {
+  users: [],
+};
+
+app.get('/', (req, res) => {
+  res.render('main');
 });
+
+app.post('/login', (req, res) => {
+  // const { name, email, password } = req.body;
+  fs.readFile('users.json', (err, data) => {
+    if (err) throw err;
+    dataFile = JSON.parse(data.toString());
+    dataFile.users.push(req.body);
+    fs.writeFile('users.json', JSON.stringify(dataFile), (error) => {
+      if (error) throw error;
+    });
+  });
+
+  res.json('OK');
+});
+
+app.listen(3000);
