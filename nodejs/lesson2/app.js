63707e388f9e90a33f4107d8681dce0d89ce2caf
Ihor Karpov <igora75@gmail.com>
2021-01-01 03:21:56 +0200

    lesson2

--- nodejs/lesson2/app.js
+++ nodejs/lesson2/app.js
@@ -1,12 +1,22 @@
+const usersArr = [
+  { name: 'Ivan', age: 22 },
+  { name: 'Ira', age: 17 },
+];
 const express = require('express');
+const expressHbs = require('express-handlebars');
+// const path = require('path');
 
 const app = express();
-
-app.get('/', (req, res) => {
-    console.log(req);
-    res.send('test');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.static(path.join(__dirname, 'views')));
+app.engine('.hbs', expressHbs({ defaultLayout: false }));
+app.set('view engine', '.hbs');
+app.get('/user', (req, res) => {
+  res.render('users', { users: usersArr });
 });
-
+// app.get('/', (req, res) => {
+//
+// });
 app.listen(3000, () => {
-    console.log('hdgththyyyfhh');
-})
+});
